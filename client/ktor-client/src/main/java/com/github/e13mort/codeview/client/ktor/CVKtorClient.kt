2bcdbffe9de26a492db72f0ae9516b4e3423e47f
Pavel Novikov <imortan@gmail.com>
2019-06-04 00:51:46 +0300

    Print CodeView output as a GET result

--- client/ktor-client/src/main/java/com/github/e13mort/codeview/client/ktor/CVKtorClient.kt
+++ client/ktor-client/src/main/java/com/github/e13mort/codeview/client/ktor/CVKtorClient.kt
@@ -1,5 +1,10 @@
 package com.github.e13mort.codeview.client.ktor
 
+import com.github.e13mort.codeview.CodeView
+import com.github.e13mort.codeview.Output
+import com.github.e13mort.codeview.backend.java.JavaBackend
+import com.github.e13mort.codeview.datasource.filesystem.FileSystemDataSource
+import com.github.e13mort.codeview.frontend.pulm.PulmFrontend
 import io.ktor.application.call
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
@@ -7,12 +12,21 @@ import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import kotlinx.coroutines.launch
 
 fun main() {
     embeddedServer(Netty) {
         routing {
             get("/") {
-                call.respondText("Hello Code View", ContentType.Text.Html)
+                CodeView(FileSystemDataSource(
+                    "/Users/pavel/work/pets/MyApplication/backend/java-backend"),
+                    PulmFrontend(), JavaBackend(), object : Output {
+                        override fun save(data: String) {
+                            launch {
+                                call.respondText(data, ContentType.Text.Plain)
+                            }
+                        }
+                    }).run()
             }
         }
     }.start(wait = true)
