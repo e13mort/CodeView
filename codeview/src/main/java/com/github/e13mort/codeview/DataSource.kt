0e0ddb72d213f47dd5ccda28f23edf3c95ba6649
Pavel Novikov <imortan@gmail.com>
2019-06-07 01:24:54 +0300

    Empty DataSource created

--- codeview/src/main/java/com/github/e13mort/codeview/DataSource.kt
+++ codeview/src/main/java/com/github/e13mort/codeview/DataSource.kt
@@ -1,8 +1,16 @@
 package com.github.e13mort.codeview
 
 import java.io.InputStream
+import java.util.*
 
 interface DataSource {
+    object EMPTY : DataSource {
+        override fun name(): String = "Empty data source"
+
+        override fun sources(): List<SourceFile> = Collections.emptyList()
+
+    }
+
     fun name(): String
 
     fun sources(): List<SourceFile>
