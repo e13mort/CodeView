61e9b84b6e7df067128b27cf0e8663df60a1e0f6
Pavel Novikov <imortan@gmail.com>
2019-06-07 01:25:52 +0300

    Ktor app configuration updated

--- client/ktor-client/src/main/java/com/github/e13mort/codeview/client/ktor/CVKtorClient.kt
+++ client/ktor-client/src/main/java/com/github/e13mort/codeview/client/ktor/CVKtorClient.kt
@@ -1,10 +1,8 @@
 package com.github.e13mort.codeview.client.ktor
 
-import com.github.e13mort.codeview.CodeView
-import com.github.e13mort.codeview.Output
-import com.github.e13mort.codeview.backend.java.JavaBackend
-import com.github.e13mort.codeview.datasource.filesystem.FileSystemDataSource
+import com.github.e13mort.codeview.*
 import com.github.e13mort.codeview.frontend.pulm.PulmFrontend
+import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
@@ -16,18 +14,41 @@ import kotlinx.coroutines.launch
 
 fun main() {
     embeddedServer(Netty) {
-        routing {
-            get("/") {
-                CodeView(FileSystemDataSource(
-                    "/Users/pavel/work/pets/MyApplication/backend/java-backend"),
-                    PulmFrontend(), JavaBackend(), object : Output {
-                        override fun save(data: String) {
-                            launch {
-                                call.respondText(data, ContentType.Text.Plain)
-                            }
-                        }
-                    }).run()
-            }
-        }
+        init(this)
     }.start(wait = true)
+}
+
+fun Application.main() { init(this) }
+
+fun init(app: Application) {
+    app.routing {
+        get("/") {
+            CodeView(DataSource.EMPTY,
+                PulmFrontend(), SampleBackend(), object : Output {
+                    override fun save(data: String) {
+                        launch {
+                            call.respondText(data, ContentType.Text.Plain)
+                        }
+                    }
+                }).run()
+        }
+    }
+}
+
+class SampleBackend : Backend {
+    override fun transformSourcesToCVClasses(files: SourceFiles): CVClasses {
+        return listOf(
+            object : CVClass {
+                override fun name(): String = "TestClass"
+
+                override fun fields(): List<CVClassField> = emptyList()
+
+                override fun methods(): List<CVMethod> = emptyList()
+
+                override fun has(property: ClassProperty): Boolean = false
+
+            }
+        )
+    }
+
 }
