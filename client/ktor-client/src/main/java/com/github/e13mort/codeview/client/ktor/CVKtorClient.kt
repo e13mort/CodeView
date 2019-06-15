1557d6725bcd3de505bb04517674be284dd530f2
Pavel Novikov <imortan@gmail.com>
2019-06-16 00:35:21 +0300

    ImageOutput class created

--- client/ktor-client/src/main/java/com/github/e13mort/codeview/client/ktor/CVKtorClient.kt
+++ client/ktor-client/src/main/java/com/github/e13mort/codeview/client/ktor/CVKtorClient.kt
@@ -4,13 +4,10 @@ import com.github.e13mort.codeview.*
 import com.github.e13mort.codeview.frontend.pulm.PulmFrontend
 import io.ktor.application.Application
 import io.ktor.application.call
-import io.ktor.http.ContentType
-import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import kotlinx.coroutines.launch
 
 fun main() {
     embeddedServer(Netty) {
@@ -24,13 +21,7 @@ fun init(app: Application) {
     app.routing {
         get("/") {
             CodeView(DataSource.EMPTY,
-                PulmFrontend(), SampleBackend(), object : Output {
-                    override fun save(data: String) {
-                        launch {
-                            call.respondText(data, ContentType.Text.Plain)
-                        }
-                    }
-                }).run()
+                PulmFrontend(), SampleBackend(), ImageOutput(this, call)).run()
         }
     }
 }
---
+++ client/ktor-client/src/main/java/com/github/e13mort/codeview/client/ktor/ImageOutput.kt
@@ -0,0 +1,33 @@
+package com.github.e13mort.codeview.client.ktor
+
+import com.github.e13mort.codeview.Output
+import io.ktor.application.ApplicationCall
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respondOutputStream
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import net.sourceforge.plantuml.SourceStringReader
+
+class ImageOutput(
+    private val pipelineContext: CoroutineScope,
+    private val call: ApplicationCall
+) : Output {
+
+    override fun save(data: String) {
+        pipelineContext.launch { writeDiagramToResponse(data) }
+    }
+
+    private suspend fun writeDiagramToResponse(data: String) {
+        call.respondOutputStream(ContentType.Image.PNG, HttpStatusCode.OK) {
+            this.let {
+                try {
+                    SourceStringReader(data).outputImage(this)
+                } finally {
+                    flush()
+                    close()
+                }
+            }
+        }
+    }
+}
