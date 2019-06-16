bccd632e5ad14b97716a7bdec70303ed6f75bc83
Pavel Novikov <imortan@gmail.com>
2019-06-17 00:59:34 +0300

    Cli-Client: 'package' option refactored to 'sources' argument; 'format' argument created

--- client/cli-client/src/main/java/CliClient.kt
+++ client/cli-client/src/main/java/CliClient.kt
@@ -1,6 +1,9 @@
 import com.github.ajalt.clikt.core.NoRunCliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.default
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.choice
 import com.github.e13mort.codeview.CodeView
 import com.github.e13mort.codeview.backend.java.JavaBackend
 import com.github.e13mort.codeview.datasource.filesystem.FileSystemDataSource
@@ -13,16 +16,22 @@ fun main(params: Array<String>) {
 }
 
 class CodeViewFactory : NoRunCliktCommand() {
-    private val packagePath: String by option("--package", help = "Package path").default(".")
+    private enum class OutputFormat {PUML, PNG}
+
+    private val sourcesPath: String by argument("sources", help = "Path to sources root").default(".")
 
     private val outputFileName: String by option("--out-name", help = "Output file name").default("output")
 
+    private val outputFormat: OutputFormat by argument("format", help = "Output format")
+        .choice(OutputFormat.PNG.name.toLowerCase() to OutputFormat.PNG, OutputFormat.PUML.name.toLowerCase() to OutputFormat.PUML)
+        .default(OutputFormat.PUML)
+
     fun createCodeView(): CodeView {
         return CodeView(
-            createDataSource(packagePath),
+            createDataSource(sourcesPath),
             createFrontend(),
             createBackend(),
-            createOutput(outputFileName)
+            createOutput(outputFileName, outputFormat)
         )
     }
 
@@ -30,7 +39,12 @@ class CodeViewFactory : NoRunCliktCommand() {
 
     private fun createFrontend() = PulmFrontend()
 
-    private fun createOutput(outputFileName: String) = SimpleFileOutput(outputFileName)
+    private fun createOutput(outputFileName: String, outputFormat: OutputFormat): SimpleFileOutput {
+        return when (outputFormat) {
+            OutputFormat.PUML -> SimpleFileOutput(outputFileName)
+            OutputFormat.PNG -> TODO()
+        }
+    }
 
     private fun createDataSource(packagePath: String) = FileSystemDataSource(packagePath)
 }
