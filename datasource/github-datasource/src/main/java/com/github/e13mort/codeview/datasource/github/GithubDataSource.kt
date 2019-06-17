7ac88da869c77498dc49ea65b3a0e09af8548b17
Pavel Novikov <imortan@gmail.com>
2019-06-18 01:51:26 +0300

    github-datasource: github api usage template

--- datasource/github-datasource/src/main/java/com/github/e13mort/codeview/datasource/github/GithubDataSource.kt
+++ datasource/github-datasource/src/main/java/com/github/e13mort/codeview/datasource/github/GithubDataSource.kt
@@ -2,6 +2,10 @@ package com.github.e13mort.codeview.datasource.github
 
 import com.github.e13mort.codeview.DataSource
 import com.github.e13mort.codeview.SourceFile
+import com.jcabi.github.Content
+import com.jcabi.github.Coordinates
+import com.jcabi.github.RtGithub
+import java.io.InputStream
 
 class GithubDataSource : DataSource {
     override fun name(): String {
@@ -9,6 +13,30 @@ class GithubDataSource : DataSource {
     }
 
     override fun sources(): List<SourceFile> {
-        return emptyList()
+        val mutableListOf = mutableListOf<SourceFile>()
+
+        RtGithub().repos().get(object: Coordinates {
+            override fun user(): String = TODO()
+
+            override fun compareTo(other: Coordinates?): Int = 0
+
+            override fun repo(): String = TODO()
+
+        }).contents().iterate(TODO(), TODO()).forEach {
+            mutableListOf.add(GithubSourceFile(it))
+        }
+
+        return mutableListOf
     }
+}
+
+private class GithubSourceFile(private val githubContent: Content) : SourceFile {
+    override fun name(): String {
+        return githubContent.path()
+    }
+
+    override fun read(): InputStream {
+        return githubContent.raw()
+    }
+
 }
