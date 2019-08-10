f8d1363a6c0f1a611994fe7659085c9620a5e4e2
yaroslavok <yaaroslavok@gmail.com>
2019-08-10 20:14:26 +0300

    fill implemented interfaces to class by visitor pattern

--- backend/java-backend/src/main/java/com/github/e13mort/codeview/backend/java/MutableClassDescriptionVisitor.java
+++ backend/java-backend/src/main/java/com/github/e13mort/codeview/backend/java/MutableClassDescriptionVisitor.java
@@ -1,5 +1,6 @@
 package com.github.e13mort.codeview.backend.java;
 
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
@@ -13,5 +14,12 @@ class MutableClassDescriptionVisitor extends VoidVisitorAdapter<MutableClassDesc
     @Override
     public void visit(FieldDeclaration n, MutableClassDescription arg) {
         arg.addField(new JavaParserFieldDescription(n.clone()));
+    }
+
+    @Override
+    public void visit(ClassOrInterfaceDeclaration n, MutableClassDescription arg) {
+        n.getImplementedTypes().forEach(interfaceDeclaration ->
+            arg.addImplementedInterface(new JavaParserInterfaceDescription(interfaceDeclaration))
+        );
     }
 }
