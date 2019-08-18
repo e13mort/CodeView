f4dc9337328fd0e1e83dee12bc3b981354305b7c
Pavel Novikov <imortan@gmail.com>
2019-08-18 21:26:22 +0300

    VisitorStoreObject's logic divided into few classes

--- frontend/pulm-frontend/src/main/java/com/github/e13mort/codeview/frontend/pulm/VisitorStoredObject.kt
+++ frontend/pulm-frontend/src/main/java/com/github/e13mort/codeview/frontend/pulm/VisitorStoredObject.kt
@@ -2,52 +2,38 @@ package com.github.e13mort.codeview.frontend.pulm
 
 import com.github.e13mort.codeview.*
 
-class VisitorStoredObject (private val classes: CVClasses) :
+class VisitorStoredObject(private val classes: CVClasses) :
     StoredObject {
 
     override fun asString(): String {
         val builder = StringBuilder()
         builder.append("$START\n")
-        classes.accept(CompositeVisitor(builder))
+        classes.accept(
+            ClassesRenderingVisitor(
+                builder,
+                MethodsRenderingVisitor(builder),
+                FieldsRenderingVisitor(builder),
+                ClassRelationsRenderingVisitor(builder)
+            )
+        )
         builder.append("$END\n")
         return builder.toString()
     }
 
-    class CompositeVisitor(private val builder: StringBuilder) :
-        CVClasses.Visitor,
-        CVClass.FieldsVisitor,
-        CVClass.MethodsVisitor,
-        CVClass.RelationVisitor {
+    class ClassesRenderingVisitor(
+        private val builder: StringBuilder,
+        private val methodsVisitor: CVClass.MethodsVisitor,
+        private val fieldsVisitor: CVClass.FieldsVisitor,
+        private val relationsRenderingVisitor: CVClass.RelationVisitor
+    ) :
+        CVClasses.Visitor {
 
         override fun onClassDetected(cls: CVClass) {
             builder.append("${chooseType(cls)} ${cls.name()} {\n")
-            cls.accept(this as CVClass.FieldsVisitor)
-            cls.accept(this as CVClass.MethodsVisitor)
+            cls.accept(fieldsVisitor)
+            cls.accept(methodsVisitor)
             builder.append("}\n")
-            cls.accept(this as CVClass.RelationVisitor)
-        }
-
-        override fun onMethodDetected(
-            self: CVClass,
-            method: CVMethod
-        ) {
-            builder.append("${method.returnType().simpleName()} ${method.name()}(")
-            val parameters = method.parameters()
-            for ((index, it) in parameters.withIndex()) {
-                builder.append("${it.type().simpleName()} ${it.name()}")
-                if (index < parameters.size - 1) {
-                    builder.append(", ")
-                }
-            }
-            builder.append(")\n")
-        }
-
-        override fun onFieldDetected(self: CVClass, field: CVClassField) {
-            builder.append("${field.type().simpleName()} ${field.name()}\n")
-        }
-
-        override fun onImplementedInterfaceDetected(self: CVClass, implementedInterface: CVClass) {
-            builder.append("${implementedInterface.name()} $IMPLEMENTS_ARROW_LEFT ${self.name()}\n")
+            cls.accept(relationsRenderingVisitor)
         }
 
         private fun chooseType(cls: CVClass): String {
@@ -59,4 +45,32 @@ class VisitorStoredObject (private val classes: CVClasses) :
 
     }
 
+    class MethodsRenderingVisitor(private val builder: StringBuilder) : CVClass.MethodsVisitor {
+
+        override fun onMethodDetected(self: CVClass, method: CVMethod) {
+            builder.append("${method.returnType().simpleName()} ${method.name()}(")
+            val parameters = method.parameters()
+            for ((index, it) in parameters.withIndex()) {
+                builder.append("${it.type().simpleName()} ${it.name()}")
+                if (index < parameters.size - 1) {
+                    builder.append(", ")
+                }
+            }
+            builder.append(")\n")
+        }
+
+    }
+
+    class FieldsRenderingVisitor(private val builder: StringBuilder) : CVClass.FieldsVisitor {
+        override fun onFieldDetected(self: CVClass, field: CVClassField) {
+            builder.append("${field.type().simpleName()} ${field.name()}\n")
+        }
+    }
+
+    class ClassRelationsRenderingVisitor(private val builder: StringBuilder) : CVClass.RelationVisitor {
+        override fun onImplementedInterfaceDetected(self: CVClass, implementedInterface: CVClass) {
+            builder.append("${implementedInterface.name()} $IMPLEMENTS_ARROW_LEFT ${self.name()}\n")
+        }
+
+    }
 }
