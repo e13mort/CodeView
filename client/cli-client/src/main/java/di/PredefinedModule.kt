e4e2669b43ec3f6c969fd53db9b115148017c69f
Pavel Novikov <imortan@gmail.com>
2020-01-28 01:22:56 +0300

    ContentStorageCacheFrontend implemented

--- client/cli-client/src/main/java/di/PredefinedModule.kt
+++ client/cli-client/src/main/java/di/PredefinedModule.kt
@@ -12,35 +12,53 @@ import com.github.e13mort.codeview.log.withTag
 import dagger.Module
 import dagger.Provides
 import java.nio.file.Path
+import javax.inject.Named
 
 @Module
 class PredefinedModule(private val rootFolder: Path) {
     companion object {
-        const val CACHE_FILE_NAME = "classes.json"
-        const val CACHE_FOLDER_NAME = "backend_cache"
+        const val CACHE_FILE_BACK_NAME = "classes.json"
+        const val CACHE_FILE_FRONT_NAME = "classes.puml"
+        const val CACHE_FOLDER_BACK_NAME = "backend_cache"
+        const val CACHE_FOLDER_FRONT_NAME = "frontend_cache"
         const val CACHE_REGISTRY_FILE_NAME = "backend_registry.json"
     }
 
     @Provides
-    fun backend(contentStorage: ContentStorage, log: Log) : Backend {
+    fun backend(@Named("backend-storage") contentStorage: ContentStorage, log: Log) : Backend {
         return ContentStorageBackendCache(
             JavaBackend().withLogs(log.withTag("java backend")),
             contentStorage,
-            CVActualSerialization(CACHE_FILE_NAME)
+            CVActualSerialization(CACHE_FILE_BACK_NAME)
         ).withLogs(log.withTag("content storage backend"))
     }
 
     @Provides
-    fun frontend(log: Log) : Frontend {
-        return PulmFrontend().withLogs(log.withTag("pulm frontend"))
+    fun frontend(@Named("frontend-storage") contentStorage: ContentStorage, log: Log) : Frontend {
+        return ContentStorageCacheFrontend(
+            PulmFrontend().withLogs(log.withTag("pulm frontend")),
+            contentStorage,
+            ContentStorageCacheFrontend.StoredObjectActualSerialization()
+        ).withLogs(log.withTag("cached frontend"))
     }
 
+    @Named("backend-storage")
     @Provides
     fun contentStorage() : ContentStorage {
         return PathBasedStorage(
-            rootFolder.resolve(CACHE_FOLDER_NAME),
+            rootFolder.resolve(CACHE_FOLDER_BACK_NAME),
             CACHE_REGISTRY_FILE_NAME,
-            ConstNameUUIDBasedCacheName(CACHE_FILE_NAME)
+            ConstNameUUIDBasedCacheName(CACHE_FILE_BACK_NAME)
+        )
+    }
+
+    @Named("frontend-storage")
+    @Provides
+    fun contentStorageFront() : ContentStorage {
+        return PathBasedStorage(
+            rootFolder.resolve(CACHE_FOLDER_FRONT_NAME),
+            CACHE_REGISTRY_FILE_NAME,
+            ConstNameUUIDBasedCacheName(CACHE_FILE_FRONT_NAME)
         )
     }
 
