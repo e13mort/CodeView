2898582740163d4e91169ea03eec5fab9370cc8f
Ilya Kyzmin <ilya.kyzmin@myoffice.team>
2020-03-13 13:39:11 +0300

    added two solutions

--- cache/cache-common/src/main/java/com/github/e13mort/codeview/cache/CachedCVTransformation.kt
+++ cache/cache-common/src/main/java/com/github/e13mort/codeview/cache/CachedCVTransformation.kt
@@ -2,6 +2,7 @@ package com.github.e13mort.codeview.cache
 
 import com.github.e13mort.codeview.CVTransformation
 import com.github.e13mort.codeview.Content
+import io.reactivex.Observable
 import io.reactivex.Single
 import java.nio.file.Path
 
@@ -23,13 +24,25 @@ class CachedCVTransformation<INPUT, OUTPUT>(
             Single.just(transformOperation)
         } else {
             //FIXME Save operation should return content of cached operation somehow. Otherwise we'll make transformation even after caching
-            save(transformOperation).map { transformOperation }
+            save(transformOperation).map { transformOperation } //OR second solution without map
         }
         //OR if we use fast hacky solution we just return Single.just(cacheResult.cachedOperation)
     }
 
-    private fun save(transformOperation: CVTransformation.TransformOperation<OUTPUT>): Single<ContentStorage.ContentStorageItem> {
+    private fun save(transformOperation: CVTransformation.TransformOperation<OUTPUT>): /*Single<CVTransformation.TransformOperation<OUTPUT>>*/Single<ContentStorage.ContentStorageItem> {
+// OR second solution, when we serialize and save result of transformation in doOnSuccess
+//        return transformOperation
+//            .transform()
+//            .doOnSuccess {
+//                storage.put(
+//                    transformOperation.description(),
+//                    Observable.fromCallable { serialization.serialize(it) }
+//
+//                )
+//            }
+//            .map { DumbTransformOperation(it, transformOperation.description()) }
         //FIXME maybe you should return content of cached operation from put method and wrap it with DumbTransformOperation
+
         return storage.put(
             transformOperation.description(),
             transformOperation
@@ -42,15 +55,15 @@ class CachedCVTransformation<INPUT, OUTPUT>(
     private fun searchForItem(sourceOperation: CVTransformation.TransformOperation<OUTPUT>): Single<CacheResult<OUTPUT>> {
         return storage
             .search(sourceOperation.description())
-           //OR we can just save this operation if we can't find it, then return result of search (fast hacky solution)
-          //  .switchIfEmpty(
+            //OR we can just save this operation if we can't find it, then return result of search (fast hacky solution)
+            //  .switchIfEmpty(
             //    save(sourceOperation)
-              //      .flatMapMaybe { storage.search(sourceOperation.description()) }
-           // )
+            //      .flatMapMaybe { storage.search(sourceOperation.description()) }
+            // )
             .map(this::deserialize)
             .map { createCacheResult(it, sourceOperation.description()) }
             .toSingle()
-            .onErrorReturn{
+            .onErrorReturn {
                 CacheResult(
                     sourceOperation,
                     false
