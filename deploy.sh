30f9dcc8759b30f8e2b631f33024f3db54ce0ba9
Pavel Novikov <imortan@gmail.com>
2020-06-05 02:43:08 +0300

    perform docker login action

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "username=$DOCKER_USERNAME"
-echo "password=$DOCKER_PASSWORD"
+docker --version
+docker login docker.pkg.github.com -u e13mort -p $DOCKER_PASSWORD
+#docker docker build -t code-view client/ktor-client
+#docker tag code-view docker.pkg.github.com/e13mort/codeview/code-view:latest
+#docker push docker.pkg.github.com/e13mort/codeview/code-view:latest
