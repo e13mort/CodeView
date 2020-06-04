2a95187ec6049515157970235162e63b7f72d1db
Pavel Novikov <imortan@gmail.com>
2020-06-05 02:46:20 +0300

    perform actual assembling steps

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 docker --version
 docker login docker.pkg.github.com -u e13mort -p $DOCKER_PASSWORD
-#docker docker build -t code-view client/ktor-client
-#docker tag code-view docker.pkg.github.com/e13mort/codeview/code-view:latest
-#docker push docker.pkg.github.com/e13mort/codeview/code-view:latest
+docker build -t code-view client/ktor-client
+docker tag code-view docker.pkg.github.com/e13mort/codeview/code-view:latest
+docker push docker.pkg.github.com/e13mort/codeview/code-view:latest
