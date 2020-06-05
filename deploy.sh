c88bb2a6696bf0b924a46646619f205f406b4e03
Pavel Novikov <imortan@gmail.com>
2020-06-06 00:22:28 +0300

    deploy to docker hub instead of github

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker login docker.pkg.github.com -u e13mort -p $DOCKER_PASSWORD
-docker push docker.pkg.github.com/e13mort/codeview/code-view:latest
+docker login -u e13mort -p $DOCKER_PASSWORD
+docker push e13mort/codeview:latest
