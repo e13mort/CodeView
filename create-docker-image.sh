79f22c609a1ca6d8a3e5e41fdaf9d4ffae8200e7
Pavel Novikov <imortan@gmail.com>
2020-06-06 00:30:47 +0300

    create docker image with correct tag

--- create-docker-image.sh
+++ create-docker-image.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 docker --version
-docker build -t code-view client/ktor-client
-docker tag code-view docker.pkg.github.com/e13mort/codeview/code-view:latest
+docker build -t e13mort/codeview:latest client/ktor-client
